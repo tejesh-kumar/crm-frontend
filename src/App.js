86b88bb11ed9a0e3211d445b541f9b3762863ce2
tejesh-kumar <tejeshmuralikrishna@gmail.com>
2022-05-15 15:38:58 +0530

    app setup, login page, change password for frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import './App.css';
+import Entry from './pages/entry/Entry.page';
+
+function App() {
+  return (
+    <div className="App">
+      <Entry />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/entry/Entry.page.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable consistent-return */
+import React, { useState } from 'react';
+import LoginForm from '../../components/login/Login.comp';
+import ResetPassword from '../../components/password-reset/PasswordReset.comp';
+import './entry.style.css';
+
+function Entry() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [frmLoad, setFrmLoad] = useState('login');
+
+  const handleOnChange = e => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        break;
+
+      case 'password':
+        setPassword(value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const handleOnSubmit = e => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      return alert('Fill up all the form!');
+    }
+
+    // TODO call api to submit the form
+    console.log(email, password);
+  };
+
+  const handleOnResetSubmit = e => {
+    e.preventDefault();
+
+    if (!email) {
+      return alert('Please enter the emial!');
+    }
+
+    // TODO call api to submit the form
+    console.log(email);
+  };
+
+  const formSwitcher = frmType => {
+    setFrmLoad(frmType);
+  };
+
+  return (
+    <div className="entry-page bg-info">
+      <div className="form-box">
+        {frmLoad === 'login' && (
+          <LoginForm
+            handleOnChange={handleOnChange}
+            handleOnSubmit={handleOnSubmit}
+            formSwitcher={formSwitcher}
+            email={email}
+            pass={password}
+          />
+        )}
+
+        {frmLoad === 'rest' && (
+          <ResetPassword
+            handleOnChange={handleOnChange}
+            handleOnResetSubmit={handleOnResetSubmit}
+            formSwitcher={formSwitcher}
+            email={email}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Entry;
