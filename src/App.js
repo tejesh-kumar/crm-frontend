e0f04720f1151d386cf8511053b13f151bb021b4
tejesh-kumar <tejeshmuralikrishna@gmail.com>
2022-05-21 09:29:47 +0530

    added ticket lists page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import DefaultLayout from './layout/DefaultLayout';
 // import Dashboard from './pages/dashboard/Dashboard.page';
 // import Entry from './pages/entry/Entry.page';
-import AddTicket from './pages/new-ticket/AddTicket.page';
+// import AddTicket from './pages/new-ticket/AddTicket.page';
+import TicketLists from './pages/ticket-list/TicketLists.page';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <DefaultLayout>
         {/* <Entry /> */}
         {/* <Dashboard /> */}
-        <AddTicket />
+        {/* <AddTicket /> */}
+        <TicketLists />
       </DefaultLayout>
     </div>
   );
